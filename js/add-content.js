60c8cfcfe3f922c54f72ef8151dda0ed44e87281
Alex2Pena <attn.Alex.P@gmail.com>
2019-12-18 21:31:45 -0800

    added JS

---
+++ js/add-content.js
@@ -0,0 +1,8 @@
+var computer = prompt ("Would you like to purchase a mac or pc")
+
+if computer == mac { alert ("Awesome")
+} 
+else if computer == pc { alert (Ewwwwwwww)
+}
+else { confirm ("Can't decide?")
+}
