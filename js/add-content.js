e0ac03e58024bcac2352a555dc3e08845c3af239
bradsmialek <bradsmilek@gmail.com>
2019-12-23 13:39:36 -0800

    changes to students work for functionality

--- js/add-content.js
+++ js/add-content.js
@@ -1,8 +1,17 @@
-var computer = prompt ("Would you like to purchase a mac or pc")
+'use strict'
+// console.log(computer);
+var computer = prompt("Would you like to purchase a mac or pc");
+console.log(computer);
+var answer;
 
-if computer == mac { alert ("Awesome")
-} 
-else if computer == pc { alert (Ewwwwwwww)
+if(computer === "mac") { 
+  // alert("Awesome");
+  answer = 'Youre the best';
+}else if(computer === "pc") { 
+  // alert("Ewwwwwwww");
+  answer= 'PCs are sooo  EWEEEEE!!';
+}else{ 
+  // alert("Can't decide?");
+  answer = 'Well, figure it out!';
 }
-else { confirm ("Can't decide?")
-}
+document.write(answer);
